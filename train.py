36ea837739e9a682f4077a1a23099d55fbccd058
Henrik G <henriarm@yandex.ru>
2023-09-16 14:20:43 +0300

    Fixed model name and folder name

--- train.py
+++ train.py
@@ -11,8 +11,10 @@ def read_flickr_logos_annotations(annotations_path):
     with open(annotations_path, "r") as f:
         lines = f.readlines()
 
-    # split each line by tab character
+    # split each line by tab character and space
     data = [line.strip().split("\t") for line in lines]
+    data = [line.strip().split(" ") for line in lines]
+
 
     # Extract individual data points from each line
     file_names, class_names, _, x1, y1, x2, y2 = zip(*data)
@@ -20,10 +22,10 @@ def read_flickr_logos_annotations(annotations_path):
     return file_names, class_names, x1, y1, x2, y2
 
 
-file_names, class_names, x1, y1, x2, y2 = read_flickr_logos_annotations("dataset/'flickr_logos_27_dataset/flickr_logos_27_dataset_training_set_annotation.txt")
+file_names, class_names, x1, y1, x2, y2 = read_flickr_logos_annotations("dataset/flickr_logos_27_dataset/flickr_logos_27_dataset_training_set_annotation.txt")
 
 # Define the dataset and transformations
-transform = transforms.Compose([transforms.ToTensor()])
+transform = transforms.Compose([transforms.ToTensor(), transforms.Resize((800, 800))])
 dataset = FlickrLogosDataset(
     "flickr_logos_27_dataset_images",
     file_names,
@@ -35,7 +37,7 @@ dataloader = DataLoader(dataset, batch_size=8, shuffle=True, num_workers=4)
 
 # Initialize the model for Faster R-CNN
 num_classes = len(set(class_names)) + 1  # +1 for the background class
-model = detection.faster_rcnn_resnet50_fpn(pretrained=False, num_classes=num_classes)
+model = detection.fasterrcnn_resnet50_fpn(pretrained=False, num_classes=num_classes)
 
 # Move model to GPU if available
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
