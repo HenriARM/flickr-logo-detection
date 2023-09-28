67d421401eddd0b1c4b5637d7e6873b2b7db49a0
Henrik G <henriarm@yandex.ru>
2023-09-28 13:59:07 +0300

    add class name to visualization

--- utils.py
+++ utils.py
@@ -33,7 +33,8 @@ def show_images(images, nrow=8):
 import numpy as np
 import matplotlib.pyplot as plt
 
-def visualize_predictions(image, predictions, score_threshold=0.5):
+
+def visualize_predictions(image, predictions, class_to_idx, score_threshold=0.5):
     """
     Visualize object detection predictions on an image.
 
@@ -45,20 +46,24 @@ def visualize_predictions(image, predictions, score_threshold=0.5):
     Returns:
         None (displays the image with bounding boxes and labels).
     """
+
+    # Create inverse mapping
+    idx_to_class = {idx: class_name for class_name, idx in class_to_idx.items()}
+
     image = image.permute(1, 2, 0).cpu().numpy()  # Convert from (C, H, W) to (H, W, C)
     image = np.clip(image, 0, 1)  # Clip to [0, 1] range
 
     fig, ax = plt.subplots(1, figsize=(10, 8))
     ax.imshow(image)
 
-    boxes = predictions['boxes']
-    labels = predictions['labels']
-    scores = predictions['scores']
+    boxes = predictions["boxes"]
+    labels = predictions["labels"]
+    scores = predictions["scores"]
 
     for box, label, score in zip(boxes, labels, scores):
         if score >= score_threshold:
             x_min, y_min, x_max, y_max = box.cpu().numpy()
-            label_text = f"Class: {label.item()}, Score: {score.item():.2f}"
+            label_text = f"Class: {idx_to_class.get(label.item(), 'Unknown')}, Score: {score.item():.2f}"
 
             # Plot bounding box
             rect = plt.Rectangle(
@@ -66,8 +71,8 @@ def visualize_predictions(image, predictions, score_threshold=0.5):
                 x_max - x_min,
                 y_max - y_min,
                 linewidth=1,
-                edgecolor='r',
-                facecolor='none'
+                edgecolor="r",
+                facecolor="none",
             )
             ax.add_patch(rect)
 
@@ -76,10 +81,10 @@ def visualize_predictions(image, predictions, score_threshold=0.5):
                 x_min,
                 y_min,
                 label_text,
-                bbox=dict(facecolor='red', alpha=0.5),
+                bbox=dict(facecolor="red", alpha=0.5),
                 fontsize=12,
-                color='white'
+                color="white",
             )
 
-    plt.axis('off')
+    plt.axis("off")
     plt.show()
